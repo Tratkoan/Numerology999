42f6367e511ddea1813ffdc4ec6fb64b0a1627e0
Tratkoan <anton.tratkovski@googlemail.com>
2018-05-26 13:22:42 +0200

    NuGet package added
    NuGet package added, target android version set to 8.1. rebuild solution.

--- Numerology999/Resources/Resource.Designer.cs
+++ Numerology999/Resources/Resource.Designer.cs
@@ -31,6 +31,24 @@ namespace Numerology999
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010003
+			public const int adSize = 2130771971;
+			
+			// aapt resource value: 0x7f010004
+			public const int adSizes = 2130771972;
+			
+			// aapt resource value: 0x7f010005
+			public const int adUnitId = 2130771973;
+			
+			// aapt resource value: 0x7f010002
+			public const int circleCrop = 2130771970;
+			
+			// aapt resource value: 0x7f010001
+			public const int imageAspectRatio = 2130771969;
+			
+			// aapt resource value: 0x7f010000
+			public const int imageAspectRatioAdjust = 2130771968;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -60,14 +78,23 @@ namespace Numerology999
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int SonucScrollView = 2131034113;
+			// aapt resource value: 0x7f070004
+			public const int SonucScrollView = 2131165188;
 			
-			// aapt resource value: 0x7f050000
-			public const int editTextUserInput = 2131034112;
+			// aapt resource value: 0x7f070000
+			public const int adjust_height = 2131165184;
 			
-			// aapt resource value: 0x7f050002
-			public const int textViewText = 2131034114;
+			// aapt resource value: 0x7f070001
+			public const int adjust_width = 2131165185;
+			
+			// aapt resource value: 0x7f070003
+			public const int editTextUserInput = 2131165187;
+			
+			// aapt resource value: 0x7f070002
+			public const int none = 2131165186;
+			
+			// aapt resource value: 0x7f070005
+			public const int textViewText = 2131165189;
 			
 			static Id()
 			{
@@ -75,6 +102,22 @@ namespace Numerology999
 			}
 			
 			private Id()
+			{
+			}
+		}
+		
+		public partial class Integer
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int google_play_services_version = 2131034112;
+			
+			static Integer()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Integer()
 			{
 			}
 		}
@@ -98,20 +141,47 @@ namespace Numerology999
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f04000a
+			public const int ApplicationName = 2130968586;
+			
+			// aapt resource value: 0x7f040009
+			public const int Hello = 2130968585;
+			
+			// aapt resource value: 0x7f04000d
+			public const int InputSize = 2130968589;
+			
+			// aapt resource value: 0x7f04000b
+			public const int TextMargin = 2130968587;
+			
+			// aapt resource value: 0x7f04000c
+			public const int TextSize = 2130968588;
+			
+			// aapt resource value: 0x7f040005
+			public const int accept = 2130968581;
+			
 			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			public const int auth_google_play_services_client_facebook_display_name = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
-			
-			// aapt resource value: 0x7f040004
-			public const int InputSize = 2130968580;
+			public const int auth_google_play_services_client_google_display_name = 2130968576;
 			
 			// aapt resource value: 0x7f040002
-			public const int TextMargin = 2130968578;
+			public const int common_google_play_services_unknown_issue = 2130968578;
+			
+			// aapt resource value: 0x7f040008
+			public const int create_calendar_message = 2130968584;
+			
+			// aapt resource value: 0x7f040007
+			public const int create_calendar_title = 2130968583;
+			
+			// aapt resource value: 0x7f040006
+			public const int decline = 2130968582;
+			
+			// aapt resource value: 0x7f040004
+			public const int store_picture_message = 2130968580;
 			
 			// aapt resource value: 0x7f040003
-			public const int TextSize = 2130968579;
+			public const int store_picture_title = 2130968579;
 			
 			static String()
 			{
@@ -119,6 +189,63 @@ namespace Numerology999
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int Theme_IAPTheme = 2131099648;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] AdsAttrs = new int[] {
+					2130771971,
+					2130771972,
+					2130771973};
+			
+			// aapt resource value: 0
+			public const int AdsAttrs_adSize = 0;
+			
+			// aapt resource value: 1
+			public const int AdsAttrs_adSizes = 1;
+			
+			// aapt resource value: 2
+			public const int AdsAttrs_adUnitId = 2;
+			
+			public static int[] LoadingImageView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970};
+			
+			// aapt resource value: 2
+			public const int LoadingImageView_circleCrop = 2;
+			
+			// aapt resource value: 1
+			public const int LoadingImageView_imageAspectRatio = 1;
+			
+			// aapt resource value: 0
+			public const int LoadingImageView_imageAspectRatioAdjust = 0;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
---
+++ Numerology999/obj/Debug/android/src/md56b964469fe52ba8bf73782bfa435747d/MainActivity.java
@@ -0,0 +1,47 @@
+package md56b964469fe52ba8bf73782bfa435747d;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Numerology999.MainActivity, Numerology999", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Numerology999.MainActivity, Numerology999", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
